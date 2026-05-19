24ddb94e5d158e88dfd07a3fedabfc887820e76f
ELIEL DAN JAMILIS <49086719@ort>
2026-05-19 10:12:40 -0300

    api key

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const API_URL =
   "https://api.thecatapi.com/v1/images/search?limit=10";
 
-const API_KEY = "TU_API_KEY";
+const API_KEY = "live_VVKAwKhXDom5EvLcC0cqfSfSrxtXZ5falPPoMeiV0IdoqKwFDs3YgEte0HzBkCzp";
 
 export const getCats = async () => {
   const response = await axios.get(
